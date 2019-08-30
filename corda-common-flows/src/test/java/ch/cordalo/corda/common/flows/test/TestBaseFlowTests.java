00ae226dfbb1d7689ff39d0f683a5610319444ca
Lorenz Hänggi <lolo8304@gmail.com>
2019-08-30 20:44:47 +0200

    fix test case to see stacktrace

--- corda-common-flows/src/test/java/ch/cordalo/corda/common/flows/test/TestBaseFlowTests.java
+++ corda-common-flows/src/test/java/ch/cordalo/corda/common/flows/test/TestBaseFlowTests.java
@@ -6,6 +6,7 @@ import ch.cordalo.corda.common.contracts.test.TestState;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.transactions.SignedTransaction;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,28 +28,42 @@ public class TestBaseFlowTests extends CordaloTestEnvironment {
     public void tearDown() { super.tearDown(); }
 
     @Test
-    public void testCreate() throws ExecutionException, InterruptedException {
-        TestBaseFlow.Create flow = new TestBaseFlow.Create(testNode2.party, "my string", 42);
-        SignedTransaction signedTransaction = testNode1.startFlow(flow);
-        StateVerifier stateVerifier = StateVerifier.fromTransaction(signedTransaction, flow.getServiceHub());
-        stateVerifier.output().one();
-
+    public void testCreate()  {
+        try {
+            TestBaseFlow.Create flow = new TestBaseFlow.Create(testNode2.party, "my string", 42);
+            SignedTransaction signedTransaction = null;
+            signedTransaction = testNode1.startFlow(flow);
+            StateVerifier stateVerifier = StateVerifier.fromTransaction(signedTransaction, flow.getServiceHub());
+            stateVerifier.output().one();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
     }
 
 
     @Test
-    public void testUpdate() throws ExecutionException, InterruptedException {
-        TestBaseFlow.Create flow = new TestBaseFlow.Create(testNode2.party, "my string", 42);
-        SignedTransaction signedTransaction = testNode1.startFlow(flow);
-        StateVerifier stateVerifier = StateVerifier.fromTransaction(signedTransaction, flow.getServiceHub());
-        TestState test = stateVerifier.output().one().object();
+    public void testUpdate() {
+        try {
+            TestBaseFlow.Create flow = new TestBaseFlow.Create(testNode2.party, "my string", 42);
+            SignedTransaction signedTransaction = testNode1.startFlow(flow);
+            StateVerifier stateVerifier = StateVerifier.fromTransaction(signedTransaction, flow.getServiceHub());
+            TestState test = stateVerifier.output().one().object();
 
-        TestBaseFlow.UpdateProvider flowU = new TestBaseFlow.UpdateProvider(test.getLinearId(), testNode3.party);
-        SignedTransaction signedTransaction2 = testNode1.startFlow(flowU);
-        StateVerifier stateVerifier2 = StateVerifier.fromTransaction(signedTransaction2, flow.getServiceHub());
-        TestState test2 = stateVerifier2.output().one().object();
-
-
+            TestBaseFlow.UpdateProvider flowU = new TestBaseFlow.UpdateProvider(test.getLinearId(), testNode3.party);
+            SignedTransaction signedTransaction2 = testNode1.startFlow(flowU);
+            StateVerifier stateVerifier2 = StateVerifier.fromTransaction(signedTransaction2, flow.getServiceHub());
+            TestState test2 = stateVerifier2.output().one().object();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
     }
 
 
