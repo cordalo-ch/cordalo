63378865cb2bc939ab79e0a60d253395584086c7
Cédric Walter <cedric.walter@gmail.com>
2019-11-03 13:38:19 +0100

    cleanup and remove skip integrationTest

--- .travis/build.sh
+++ .travis/build.sh
@@ -12,11 +12,5 @@ else
 	echo "Not a manual release"
 fi
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ];
-then
-	echo "Skip integration tests in pull request builds"
-	./gradlew clean build -x integrationTest
-else
-	echo "Temporarily skip integration tests in all builds. Too heavy for Travis"
-	./gradlew clean build -x integrationTest
-fi
+# to skip ./gradlew clean build -x integrationTest
+./gradlew clean build
