3831aa6d2a64ee661f4f3fad3b7f762aabf04021
Cédric Walter <cedric.walter@gmail.com>
2019-11-06 08:00:54 +0100

    Revert "new way to push to nexus and build release"
    This reverts commit ac28fac93370b05e4cf9cc2248aa3b79c79b1bc6.

--- .travis/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
-then
-    openssl aes-256-cbc -K $encrypted_fca62744a4cd_key -iv $encrypted_fca62744a4cd_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d
-fi
