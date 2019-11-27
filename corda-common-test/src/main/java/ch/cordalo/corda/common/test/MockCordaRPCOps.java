5f34c05d2746772d5c435b09f0278fcb98c0dd45
Cédric Walter <cedric.walter@gmail.com>
2019-11-27 10:18:46 +0100

    update to corda 4.3

--- corda-common-test/src/main/java/ch/cordalo/corda/common/test/MockCordaRPCOps.java
+++ corda-common-test/src/main/java/ch/cordalo/corda/common/test/MockCordaRPCOps.java
@@ -1,4 +1,4 @@
-/*
+/*******************************************************************************
  * Copyright (c) 2019 by cordalo.ch - MIT License
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -6,7 +6,7 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+ ******************************************************************************/
 package ch.cordalo.corda.common.test;
 
 import net.corda.core.concurrent.CordaFuture;
@@ -19,12 +19,13 @@ import net.corda.core.identity.CordaX500Name;
 import net.corda.core.identity.Party;
 import net.corda.core.messaging.*;
 import net.corda.core.node.NetworkParameters;
+import net.corda.core.node.NodeDiagnosticInfo;
 import net.corda.core.node.NodeInfo;
 import net.corda.core.node.services.NetworkMapCache;
 import net.corda.core.node.services.Vault;
 import net.corda.core.node.services.vault.*;
 import net.corda.core.transactions.SignedTransaction;
-import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.lang3.NotImplementedException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import rx.Observable;
@@ -363,5 +364,11 @@ public class MockCordaRPCOps implements CordaRPCOps {
     @Override
     public int getProtocolVersion() {
         return this.env.node.getInfo().getPlatformVersion();
+    }
+
+    @NotNull
+    @Override
+    public NodeDiagnosticInfo nodeDiagnosticInfo() {
+        throw new NotImplementedException("#nodeDiagnosticInfo not supported yet");
     }
 }
