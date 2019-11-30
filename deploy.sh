169b54cc2e8c06694d9cd1ad1d29eaf5f699fe22
Lorenz Hänggi <lolo8304@gmail.com>
2019-11-30 18:05:27 +0100

    fix travis for sign archives

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@
   openssl aes-256-cbc -K $encrypted_fca62744a4cd_key -iv $encrypted_fca62744a4cd_iv -in secret-ring.gpg.enc -out secret-ring.gpg -d
   DIR=`pwd`
   cat ${DIR}/secret-ring.gpg | awk 'NR == 1 { print "signingKey=" } 1' ORS='\\n' > gradle.properties
-  ./gradlew --info uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${DIR}/secret-ring.gpg
+  ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${DIR}/secret-ring.gpg
   rm ${DIR}/secret-ring.gpg
+  rm ${DIR}/gradle.properties
   SIGN_KEY=none
  fi
