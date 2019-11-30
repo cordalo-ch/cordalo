02ac4ec646ada5b38cbc86c3e41e3e3beabdac45
Lorenz Hänggi <lolo8304@gmail.com>
2019-11-30 08:58:59 +0100

    fix travis

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
  #!/usr/bin/env bash
+
+ ## only upload archives for "master" so far - must be adapted later if possible for all branches
  if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=`pwd`/secret-ring.gpg
  fi
