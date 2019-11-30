e88cbe6deff4fe34e7faf4ec3205463afc9dfa6a
Lorenz Hänggi <lolo8304@gmail.com>
2019-11-30 17:48:33 +0100

    fix travis for sign archives

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@
  ## you have to export the key again
  if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_fca62744a4cd_key -iv $encrypted_fca62744a4cd_iv -in secret-ring.gpg.enc -out secret-ring.gpg -d
-  ./gradlew --info uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=`pwd`/secret-ring.gpg
-  rm `pwd`/secret-ring.gpg
+  DIR=`pwd`
+  SIGN_KEY=`cat ${DIR}/sign.gpg | awk 'NR == 1 { print "" } 1' ORS='\\n'`
+  ./gradlew --info uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${DIR}/secret-ring.gpg -PsigningKey=${SIGN_KEY}
+  rm ${DIR}/secret-ring.gpg
+  SIGN_KEY=none
  fi
