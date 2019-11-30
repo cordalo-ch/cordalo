9e608b3369a5238219f9d33005d37cecf704bd40
Lorenz Hänggi <lolo8304@gmail.com>
2019-11-30 18:01:46 +0100

    fix travis for sign archives

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@
  if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_fca62744a4cd_key -iv $encrypted_fca62744a4cd_iv -in secret-ring.gpg.enc -out secret-ring.gpg -d
   DIR=`pwd`
-  SIGN_KEY=`cat ${DIR}/secret-ring.gpg | awk 'NR == 1 { print "" } 1' ORS='\\n'`
-  ./gradlew --info uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${DIR}/secret-ring.gpg -PsigningKey=${SIGN_KEY}
+  cat ${DIR}/secret-ring.gpg | awk 'NR == 1 { print "signingKey=" } 1' ORS='\\n' > gradle.properties
+  ./gradlew --info uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=${DIR}/secret-ring.gpg
   rm ${DIR}/secret-ring.gpg
   SIGN_KEY=none
  fi
